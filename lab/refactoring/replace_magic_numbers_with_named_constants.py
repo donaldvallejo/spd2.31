ff300e2822fb62587884bd0d7994fc7186f3b985
donald vallejo <donny.vallejo@gmail.com>
2021-02-22 23:27:39 -0800

    finished superclass

--- lab/refactoring/replace_magic_numbers_with_named_constants.py
+++ lab/refactoring/replace_magic_numbers_with_named_constants.py
@@ -3,10 +3,10 @@
 
 # First Section
 # Given two point charges, calcualte the electric force exerted on them.
-q1 = int(input('Enter a value of charge q1: '))
-q2 = int(input('Enter a value of charge q2: '))
-distance = int(input("Enter the distance be10tween two charges: "))
-force = 8.9875517923*1e9 * q1 * q2/(distance**2)
+question_1 = int(input('Enter a value of charge q1: '))
+question_2 = int(input('Enter a value of charge q2: '))
+distance = int(input("Enter the distance between two charges: "))
+force = 8.9875517923*1e9 * question_1 * question_2/(distance**2)
 print ("Electric Force between q1 and q2 is: ", force, "Newton")
 # Second Section
 num = int(input('Enter an integer number: '))

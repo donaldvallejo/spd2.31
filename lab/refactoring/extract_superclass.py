0562edc771939cbf78e51a7b2e753623d8e272c3
donald vallejo <donny.vallejo@gmail.com>
2021-02-22 23:23:05 -0800

    finished superclass

--- lab/refactoring/extract_superclass.py
+++ lab/refactoring/extract_superclass.py
@@ -1,54 +1,49 @@
-class Shape:
-
-    def __init__(self, x, y, visible = True):
-        self.x = x
-        self.y = y
-        self.visible = visible
-
-    def hide(self):
-        self.visible = False
-
-    def make_visible(self):
-        self.visible = True
-
-
-class Circle(Shape):
-
-    def __init__(self, x, y, r, visible = True):
-        super().__init__(x, y, visible)
-        self.r = r
-
-    def display(self):
-        if self.visible:
-            print('drew the circle.')
-
-    def get_center(self):
-        return self.x, self.y
+# By Kamran Bigdely
+# Extract superclass
+class AngryMushroom:
+    
+    def __init__(self):
+        self.health = 100
+        
+    def take_damage(self, damage):
+        self.health -= damage
+        
+    def spread_poison(self):
+        print('spreading poison!')
 
 
-class Rectangle(Shape):
+class AngryBot:
+    
+    def __init__(self):
+        self.health = 100
+        self.n_bullets = 40
+        
+    def take_damage(self, damage):
+        self.health -= damage
+        
+    def punch_iron_fist(self):
+        print("punching with iron fist!")
+        
+    def shoot(self):
+        print("shot a bullet!")
+        self.n_bullets -= 1
 
-    def __init__(self, x, y, width, height, visible = True):
-        super().__init__(x, y, visible)
-        self.width = width
-        self.height = height
 
-    def display(self):
-        if self.visible:
-            print('drew the rectable.')
 
-    def get_center(self):
-        return self.x + self.width/2, self.y + self.height/2
+class AgressiveAlligator:
+    
+    def __init__(self):
+        self.health = 100
+        
+    def take_damage(self, damage):
+        self.health -= damage
+        
+    def bite(self):
+        print('bitting!')
 
-if __name__ == "__main__":
-    circle = Circle(0, 0, 10, False)
-    circle.make_visible()
-    circle.display()
-    print('center point',circle.get_center())
 
-    rect = Rectangle(10, 10, 20, 5)
-    rect.hide()
-    rect.display() # does not display because it's hidden.
-    rect.make_visible()
-    rect.display()
-    print('center point',rect.get_center())
+angryMushroom = AngryMushroom()
+print("initial health level:", angryMushroom.health)
+angryMushroom.take_damage(25)
+print("took damage!")
+print("current health level:",angryMushroom.health)
--- lab/refactoring/extract_superclass2.py
+++ lab/refactoring/extract_superclass2.py
@@ -1,52 +1,48 @@
-# by Kami Bigdely
-# Extract superclass.
-class Circle:
-    
-    def __init__(self, x, y, r, visible = True):
-      self.center_x = x
-      self.center_y = y
-      self.r = r
-      self.visible = visible
-      
-    def display(self):
-        print('drew the circle.')
-        
-    def set_visible(self,is_visible):
-        self.visible = is_visible
-        
-    def get_center(self):
-        return self.center_x, self.center_y
-    
-    
-class Rectangle:
-    
-    def __init__(self, x, y, width, height, visible = True):
-        # left-bottom corner.
+class Shape:
+
+    def __init__(self, x, y, visible = True):
         self.x = x
         self.y = y
+        self.visible = visible
+
+    def hide(self):
+        self.visible = False
+
+    def make_visible(self):
+        self.visible = True
+
+
+class Circle(Shape):
+
+    def __init__(self, x, y, r, visible = True):
+        super().__init__(x, y, visible)
+        self.r = r
+
+    def display(self):
+        if self.visible:
+            print('drew the circle.')
+
+    def get_center(self):
+        return self.x, self.y
+
+
+class Rectangle(Shape):
+
+    def __init__(self, x, y, width, height, visible = True):
+        super().__init__(x, y, visible)
         self.width = width
         self.height = height
-        self.visible = visible
-        
+
     def display(self):
         if self.visible:
             print('drew the rectable.')
-            
-    def hide(self):
-        self.visible = False
-        
-    def make_visible(self):
-        self.visible = True
-        
+
     def get_center(self):
-        return self.x + self.width/2, \
-               self.y + self.height/2 
-
-
+        return self.x + self.width/2, self.y + self.height/2
 
 if __name__ == "__main__":
-    circle = Circle(0,0,10, False)
-    circle.set_visible(True)
+    circle = Circle(0, 0, 10, False)
+    circle.make_visible()
     circle.display()
     print('center point',circle.get_center())
 
